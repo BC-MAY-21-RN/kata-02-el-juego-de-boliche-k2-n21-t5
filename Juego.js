306f5f75490ce4b10932cd079b88498aef5923d8
danthek <abraham8507@gmail.com>
2021-11-26 18:37:44 -0600

    BOWLING GAME FUNCTIONAL BOWLING GAME WITH 10 FRAMES, BONUS ROLLS  AND APPLYING RULES
    Co-authored-by: danthek <abraham8507@gmail.com>
    Co-authored-by: Dylanson25 <dvillarreal0@ucol.mx>
    Co-authored-by: Darkboy5120 <hmaldonado0@ucol.mx>
    Co-authored-by: xFerchoVJ <fer_elsword@hotmail.com>

--- Juego.js
+++ Juego.js
@@ -4,6 +4,8 @@ class Frame {
   constructor() {
     this.roll1 = null;
     this.roll2 = null;
+    //this.rollExtra = null;
+
     this.score = 0;
   }
   get_roll1() {
@@ -13,22 +15,42 @@ class Frame {
     return this.roll2;
   }
   is_spray() {
-    //validar si en roll 1 fue chuza
-    return (this.roll1 + this.roll2 === 10) ? true : false;
+    return this.roll1 + this.roll2 === 10 ? true : false;
   }
   is_strike() {
-    return (this.roll1 === 10) ? true : false;
+    return this.roll1 === 10 ? true : false;
   }
   set_score(puntos) {
     this.score = puntos;
   }
-  play() {
-    this.roll1 = Math.floor(Math.random() * 11);
-    this.roll2 = Math.floor(Math.random() * (11 - this.roll1)); //
+  get_first_roll() {
+    return Math.floor(Math.random() * 11);
+  }
+  get_second_roll() {
+    return Math.floor(Math.random() * (11 - this.roll1));
+  }
+
+  play_fframe(){
+    this.roll1 = this.get_first_roll();
+    this.roll2 = this.get_first_roll();
     this.score = this.roll1 + this.roll2;
   }
-  sum_score(masPun){
-    this.score = this.roll1 + masPun;
+
+  bonus_roll() {
+    this.roll2 = null;
+    this.score = this.roll1;
+  }/*this.roll1 = Math.floor(Math.random() * 11);
+  this.roll2 = Math.floor(Math.random() * (11 )); //
+  this.score = this.roll1 + this.roll2;
+  mas_pin(){
+  }*/
+  play() {
+    this.roll1 = this.get_first_roll();
+    this.roll2 = this.get_second_roll();
+    this.score = this.roll1 + this.roll2;
+  }
+  sum_score(masPun) {
+    this.score += masPun;
   }
   get_score() {
     return this.score;
@@ -44,32 +66,57 @@ class Game {
   }
 
   score() {
+    //nose, estoy pensando
     for (let i = 0; i < this.initial_frames; i++) {
-      //creacion de la variable roll randomizada
       let frame = new Frame();
       frame.play();
 
+      if (i == 9) {
+        if (frame.is_strike()
+          || frame.is_spray()) {
+            this.initial_frames += 1;
+          }
+      } 
+
+
+      //si ya ha pasado al menos un frame en el juego y que no entre al ultimo
       if (i > 0) {
-        if (this.frames[i-1].is_spray()) {
-          this.frames[i-1].sum_score(frame.get_roll1());
-          console.log(this.final_score);
-          this.final_score += frame.get_roll1();
-        } /*else if (this.frames[i-1].is_strike()) {         
-        }*/
+        if (this.frames[i - 1].is_strike()) {
+          //ganamos frame extra si hacemos chuza en turno 10
+          if (i == 10) {
+            frame.play_fframe();
+          }
+         
+          //se actualiza la puntuacion del score del las 2 tiradas del frame anterior
+          this.frames[i - 1].sum_score(frame.get_roll1() + frame.get_roll2());
+          //se actualiza el score final con el bonus del strike
+          this.final_score = this.frames[i - 1].get_score();
+        } else if (this.frames[i - 1].is_spray()) {
+          
+          if (i == 10) {
+            frame.bonus_roll();
+          }
+
+          //se toma como bonus la tirada anterior
+          this.frames[i - 1].sum_score(frame.get_roll1());
+
+          this.final_score = this.frames[i - 1].get_score();
+        }
       }
-      
-      
+
       this.final_score += frame.get_score();
       frame.set_score(this.final_score);
 
       this.frames.push(frame);
     }
+
+    //aqui
     console.table(this.frames);
     console.log(this.final_score);
   }
 }
 
-let juego = new Game();
+let juego1 = new Game();
 
-//SPARE= SACA 10 CON 2 INTENTOS Y SUMA EL SIGUIENTE ROL AL SCORE
-//STRIKE= SACA 10 CON EL PRIMER INTENTO Y SUMA LOS 2 SIGUIENTES ROLS AL SCORE
+
+
